fe06959ef7946cbeb99f0fc873cef15ab37e6f5a
shuaiqing <s_q@zju.edu.cn>
2020-08-11 16:50:44 +0800

    修改路径

--- test/mocap.cpp
+++ test/mocap.cpp
@@ -4,40 +4,42 @@
 #include "openpose.h"
 #include <opencv2/opencv.hpp>
 #include <Eigen/Eigen>
+#include <unordered_map>
 #include <json/json.h>
 #include "Timer.hpp"
-// #define vis2d
+#define vis2d
 #define write3d
 std::string toOutPath(fs::path root, int frame, std::string posix){
     auto frameid = std::to_string(frame);
     return (root/fs::path(frameid + posix)).string();
 }
 
-int main()
+std::string basename(std::string path){
+    return fs::path(path).filename().string();
+}
+
+int main(int argc, char *argv[])
 {
-    cv::FileStorage fs;
-    fs.open("../config/config.yml", cv::FileStorage::READ);
-    std::string setting_, dataPath, videoposix;
-    fs["setting"] >> setting_;
-    fs["path"] >> dataPath;
-    fs["videoposix"] >> videoposix;
-
-    std::cout << "experiment: " << setting_ << std::endl;
-    std::cout << "path: " << dataPath << std::endl;
-
-    auto setting = fs::path(setting_);
-    auto outPath = fs::path("../output")/setting;
-    fs::create_directories(outPath);
-    auto detectPath = outPath/fs::path("detect");
-    auto associPath = outPath/fs::path("associate");
-    auto projecPath = outPath/fs::path("reproj");
-    auto pointsPath = outPath/fs::path("keypoints");
-    fs::create_directories(detectPath);
-    fs::create_directories(associPath);
-    fs::create_directories(projecPath);
-    fs::create_directories(pointsPath);
-
-    std::map<std::string, Camera> cameras = ParseCameras(dataPath + "calibration.json");
+    fs::path inp_path;
+    if(argc > 1) {
+        inp_path = fs::path(std::string(argv[1]));
+    } else {
+        std::cout << ">>> Please specify the input path！" << std::endl;
+        return 0;
+    }
+    auto seqname = inp_path.filename();
+    auto dataroot = inp_path.parent_path().parent_path();
+    auto outroot = dataroot/fs::path("association_out")/seqname;
+    std::cout << "experiment: " << seqname << std::endl;
+    std::cout << "experiment: " << dataroot << std::endl;
+    std::vector<std::string> outlist = {"detect", "associate", "reproj", "keypoints"};
+    std::unordered_map<std::string, fs::path> output_path;
+    for(auto name: outlist){
+        output_path[name] = outroot/fs::path(name);
+        fs::create_directories(output_path[name]);
+    }
+    std::string videoposix = ".mp4";
+    std::map<std::string, Camera> cameras = ParseCameras((inp_path/fs::path("calibration.json")).string());
     Eigen::Matrix3Xf projs(3, cameras.size() * 4);
     std::vector<cv::Mat> rawImgs(cameras.size());
     std::vector<cv::VideoCapture> videos(cameras.size());
@@ -48,11 +50,11 @@ int main()
 #pragma omp parallel for
     for (int i = 0; i < cameras.size(); i++) {
         auto iter = std::next(cameras.begin(), i);
-        videos[i] = cv::VideoCapture(dataPath + "video/" + iter->first + videoposix);
+        videos[i] = cv::VideoCapture((inp_path/fs::path("video")/fs::path(iter->first + videoposix)).string());
         videos[i].set(cv::CAP_PROP_POS_FRAMES, 0);
 
         projs.middleCols(4 * i, 4) = iter->second.eiProj;
-        seqDetections[i] = ParseDetections(dataPath + "detection/" + iter->first + ".txt");
+        seqDetections[i] = ParseDetections((inp_path/fs::path("detection")/fs::path(iter->first + ".txt")).string());
         cv::Size imgSize(int(videos[i].get(cv::CAP_PROP_FRAME_WIDTH)), int(videos[i].get(cv::CAP_PROP_FRAME_HEIGHT)));
         for (auto&&detection : seqDetections[i]) {
             for (auto&& joints : detection.joints) {
@@ -129,11 +131,11 @@ int main()
                 skelPainter.DrawReproj(skel3d.second, projs.middleCols(4 * view, 4), reprojImg(rois[view]), skel3d.first);
         }
 
-        cv::imwrite(toOutPath(detectPath, frameIdx, ".jpg"), detectImg, 
+        cv::imwrite(toOutPath(output_path["detect"], frameIdx, ".jpg"), detectImg, 
             {cv::IMWRITE_JPEG_QUALITY, 50});
-        cv::imwrite(toOutPath(associPath, frameIdx, ".jpg"), assocImg,
+        cv::imwrite(toOutPath(output_path["associate"], frameIdx, ".jpg"), assocImg,
             {cv::IMWRITE_JPEG_QUALITY, 50});
-        cv::imwrite(toOutPath(projecPath, frameIdx, ".jpg"), reprojImg,
+        cv::imwrite(toOutPath(output_path["reproj"], frameIdx, ".jpg"), reprojImg,
             {cv::IMWRITE_JPEG_QUALITY, 50});
 #endif
 
@@ -141,7 +143,7 @@ int main()
 		int nJoints = 15;
         std::cout << std::to_string(frameIdx) << std::endl;
         // 输出三维关键点
-        std::ofstream resout(toOutPath(pointsPath, frameIdx, ".txt"));
+        std::ofstream resout(toOutPath(output_path["keypoints"], frameIdx, ".txt"));
         resout << skelUpdater.GetSkel3d().size() << std::endl;
         for(const auto& skel3d : skelUpdater.GetSkel3d()){
             auto trackId = skel3d.first;
@@ -152,7 +154,7 @@ int main()
         }
 		resout.close();
         // 输出每个检测关节的指派结果
-        std::ofstream asscocOut(toOutPath(pointsPath, frameIdx, "_assign.txt"));
+        std::ofstream asscocOut(toOutPath(output_path["keypoints"], frameIdx, "_assign.txt"));
 
         for(int view=0;view<cameras.size();view++){
             for(int nj=0;nj<15;nj++){
