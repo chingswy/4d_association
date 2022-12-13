81c9053ff31e7cbaced45878bd38d8bf23c986bf
QingShuai@zju3dv <s_q@zju.edu.cn>
2022-12-13 21:34:40 +0800

    update cmdline

--- mocap/main.cpp
+++ mocap/main.cpp
@@ -6,27 +6,78 @@
 #include <Eigen/Eigen>
 #include <json/json.h>
 
+std::string toOutPath(fs::path root, int frame, std::string posix){
+    auto frameid = std::to_string(frame);
+    return (root/fs::path(frameid + posix)).string();
+}
 
-int main()
+std::string num2string(const int value, const unsigned precision)
 {
-	const std::string dataset = "seq_3";
-	std::map<std::string, Camera> cameras = ParseCameras("../data/" + dataset + "/calibration.json");
+     std::ostringstream oss;
+     oss << std::setw(precision) << std::setfill('0') << value;
+     return oss.str();
+}
+
+enum DataMode{
+	VIDEO = 0,
+	IMAGE = 1,
+};
+
+int main(int argc, char *argv[])
+{
+	std::string dataset;
+	if(argc > 1) {
+        dataset = std::string(argv[1]);
+    } else {
+        std::cout << ">>> Please specify the input path！" << std::endl;
+        return 0;
+    }
+	DataMode mode = IMAGE;
+
+	const std::vector<std::string> camlist = {"01", "03", "05", "07", "09", "11", "13", "15", "17", "19", "21", "23"};
+	const std::vector<std::string> camvis = {"01", "07", "13", "19"};
+	std::map<std::string, Camera> cameras_all = ParseCameras("../data/" + dataset + "/calibration.json");
+	// 只保留部分cameras
+	std::map<std::string, Camera> cameras;
+	for(auto cam: camlist){
+		cameras[cam] = cameras_all[cam];
+	}
+	auto inp_path = fs::path("../data")/dataset;
+	auto outroot = fs::path("../association_out")/dataset;
+	std::vector<std::string> outlist = {"detect", "associate", "reproj", "keypoints"};
+    std::unordered_map<std::string, fs::path> output_path;
+    for(auto name: outlist){
+        output_path[name] = outroot/fs::path(name);
+        fs::create_directories(output_path[name]);
+    }
+
 	Eigen::Matrix3Xf projs(3, cameras.size() * 4);
 	std::vector<cv::Mat> rawImgs(cameras.size());
 	std::vector<cv::VideoCapture> videos(cameras.size());
+	std::vector<fs::path> images_path(cameras.size());
 	std::vector<std::vector<OpenposeDetection>> seqDetections(cameras.size());
-	const SkelDef& skelDef = GetSkelDef(SKEL19);
+	const SkelDef& skelDef = GetSkelDef(SKEL15);
 	std::vector<std::map<int, Eigen::Matrix4Xf>> skels;
 
 #pragma omp parallel for
 	for (int i = 0; i < cameras.size(); i++) {
 		auto iter = std::next(cameras.begin(), i);
-		videos[i] = cv::VideoCapture("../data/" + dataset + "/video/" + iter->first + ".avi");
-		videos[i].set(cv::CAP_PROP_POS_FRAMES, 0);
+		cv::Size imgSize;
+		if(mode == DataMode::VIDEO){
+			videos[i] = cv::VideoCapture("../data/" + dataset + "/video/" + iter->first + ".mp4");
+			videos[i].set(cv::CAP_PROP_POS_FRAMES, 0);
+			imgSize = cv::Size(int(videos[i].get(cv::CAP_PROP_FRAME_WIDTH)), int(videos[i].get(cv::CAP_PROP_FRAME_HEIGHT)));
+		}else{
+			// reading the images
+			images_path[i] = inp_path/fs::path("images")/fs::path(iter->first);
+			std::string imgpath = images_path[i]/"000000.jpg";
+			cv::Mat img = cv::imread(imgpath);
+			imgSize = img.size();
+		}
 
+		std::cout << "view: " << iter->first << " imgSize: " << imgSize << std::endl;
 		projs.middleCols(4 * i, 4) = iter->second.eiProj;
 		seqDetections[i] = ParseDetections("../data/" + dataset + "/detection/" + iter->first + ".txt");
-		cv::Size imgSize(int(videos[i].get(cv::CAP_PROP_FRAME_WIDTH)), int(videos[i].get(cv::CAP_PROP_FRAME_HEIGHT)));
 		for (auto&& detection : seqDetections[i]) {
 			for (auto&& joints : detection.joints) {
 				joints.row(0) *= (imgSize.width - 1);
@@ -36,7 +87,7 @@ int main()
 		rawImgs[i].create(imgSize, CV_8UC3);
 	}
 
-	KruskalAssociater associater(SKEL19, cameras);
+	KruskalAssociater associater(SKEL15, cameras);
 	associater.SetMaxTempDist(0.3f);
 	associater.SetMaxEpiDist(0.15f);
 	associater.SetEpiWeight(1.f);
@@ -49,9 +100,10 @@ int main()
 	associater.SetNodeMultiplex(true);
 	associater.SetNormalizeEdge(true);			// new feature
 
-	SkelPainter skelPainter(SKEL19);
+	SkelPainter skelPainter(SKEL15);
 	skelPainter.rate = 512.f / float(cameras.begin()->second.imgSize.width);
-	SkelFittingUpdater skelUpdater(SKEL19, "../data/skel/SKEL19_new");
+	// SkelFittingUpdater skelUpdater(SKEL19, "../data/skel/SKEL19");
+	SkelFittingUpdater skelUpdater(SKEL15, "../data/skel/SKEL15");
 	skelUpdater.SetTemporalTransTerm(1e-1f / std::pow(skelPainter.rate, 2));
 	skelUpdater.SetTemporalPoseTerm(1e-1f / std::pow(skelPainter.rate, 2));
 	cv::Mat detectImg, assocImg, reprojImg;
@@ -59,24 +111,29 @@ int main()
 	for (int frameIdx = 0; ; frameIdx++) {
 		bool flag = true;
 		for (int view = 0; view < cameras.size(); view++) {
-			videos[view] >> rawImgs[view];
+			if(mode == DataMode::VIDEO){
+				videos[view] >> rawImgs[view];
+			}else{
+				auto imgname = (images_path[view]/fs::path(num2string(frameIdx, 6) + ".jpg")).string();
+				rawImgs[view] = cv::imread(imgname);
+			}
 			if (rawImgs[view].empty()) {
+				std::cout << "frameIdx: " << frameIdx << " view: " << view << " empty" << std::endl;
 				flag = false;
 				break;
 			}
 			cv::resize(rawImgs[view], rawImgs[view], cv::Size(), skelPainter.rate, skelPainter.rate);
-			associater.SetDetection(view, seqDetections[view][frameIdx].Mapping(SKEL19));
+			associater.SetDetection(view, seqDetections[view][frameIdx].Mapping(SKEL15));
 		}
 		if (!flag)
 			break;
-
 		associater.SetSkels3dPrev(skelUpdater.GetSkel3d());
 		associater.Associate();
 		skelUpdater.Update(associater.GetSkels2d(), projs);
 
 		
 		// save
-		const int layoutCols = 3;
+		const int layoutCols = std::sqrt(cameras.size()) + 0.5;
 		std::vector<cv::Rect> rois = SkelPainter::MergeImgs(rawImgs, detectImg, layoutCols,
 			{ rawImgs.begin()->cols, rawImgs.begin()->rows});
 		detectImg.copyTo(assocImg);
@@ -84,7 +141,7 @@ int main()
 
 #pragma omp parallel for
 		for (int view = 0; view < cameras.size(); view++) {
-			const OpenposeDetection detection = seqDetections[view][frameIdx].Mapping(SKEL19);
+			const OpenposeDetection detection = seqDetections[view][frameIdx].Mapping(SKEL15);
 			skelPainter.DrawDetect(detection.joints, detection.pafs, detectImg(rois[view]));
 			for (const auto& skel2d : associater.GetSkels2d())
 				skelPainter.DrawAssoc(skel2d.second.middleCols(view * skelDef.jointSize, skelDef.jointSize), assocImg(rois[view]), skel2d.first);
@@ -94,9 +151,9 @@ int main()
 		}
 
 		skels.emplace_back(skelUpdater.GetSkel3d());
-		cv::imwrite("../output/detect/" + std::to_string(frameIdx) + ".jpg", detectImg);
-		cv::imwrite("../output/assoc/" + std::to_string(frameIdx) + ".jpg", assocImg);
-		cv::imwrite("../output/reproj/" + std::to_string(frameIdx) + ".jpg", reprojImg);
+		cv::imwrite(toOutPath(output_path["detect"], frameIdx, ".jpg"), detectImg);
+		cv::imwrite(toOutPath(output_path["associate"], frameIdx, ".jpg"), assocImg);
+		cv::imwrite(toOutPath(output_path["reproj"], frameIdx, ".jpg"), reprojImg);
 		std::cout << std::to_string(frameIdx) << std::endl;
 	}
 
